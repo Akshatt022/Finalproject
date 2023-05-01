f2a6b65b70671b0e1d58684de1e23c8cc1240439
akshat singh <akshatsingh22032004@gmail.com>
2023-05-01 18:16:42 +0530

    all commplete

--- script.js
+++ script.js
@@ -40,7 +40,17 @@ gsap.to("#page2 img",{
    }
 })
 
-
+gsap.to("#main", {
+  backgroundColor: "#111",
+  scrollTrigger: {
+      scroller: "#main",
+      trigger: "#page2",
+      start: "top -100%",
+      end: "top -100%",
+      // markers: true,
+      scrub: 3
+  }
+})
 
 var overlay = document.querySelector("#overlay")
 var iscroll = document.querySelector("#scroll")
@@ -70,57 +80,125 @@ page3.addEventListener("mousemove",function(ee){
   scroll1.style.top = `${ee.y}px`
 })
 
+$('#page1 h1').textillate({
+  in: {
+      effect: 'fadeInUp',
+      delayScale: 1,
+  }
+});
 
-var t1 = gsap.timeline({
-  scrollTrigger:{
-    trigger:"svg",
-    scroller:"#main",
-    start:"top 45%",
-    end:"top -50%",
-    scrub:true,
-}
+gsap.from("#page2 h1", {
+  duration: 0.5,
+  onStart: function () {
+      $('#page2 h1').textillate({
+          in: {
+              effect: 'bounceIn',
+              delayScale: 0.5,
+          }
+      });
+  },
+  scrollTrigger: {
+      trigger: "#page2 h1",
+      scroller: "#main",
+      start: "top 90%"
+  }
 })
-t1.to("svg",{
-  scale:1,
-  top:"5%",
-  fill:"#111",
- 
-}) 
 
-t1.to("#nav",{
-  color:"#111",
+var tl = gsap.timeline({
+  scrollTrigger: {
+      trigger: "svg",
+      scroller: "#main",
+      // markers: true,
+      start: "top 0%",
+      end: "top -200%",
+      scrub: true,
+  }
+})
+
+tl.to("svg", {
+  scale: 1,
+  top: "5%",
+  fill: "#111",
+
+})
+
+tl.to("#nav", {
+  color: "#111",
   background: "linear-gradient(#ffffffeb,#ffffff6e,#ffffff00)",
 })
 
-var t2 = gsap.timeline({
-  scrollTrigger:{
-    trigger:"svg",
-    scroller:"#main",
-    start:"top -350%",
-    end:"top -350%",
-    scrub:true,
-}
+// Make a timeline again to change color of nav and svg to white
+
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+      trigger: "svg",
+      scroller: "#main",
+      // markers: "true",
+      start: "top -340%",
+      end: "top -340%",
+      scrub: true,
+  }
+})
+tl2.to("svg", {
+  scale: 1,
+  top: "5%",
+  fill: "#fff",
+
+})
+tl2.to("#nav", {
+  color: "#fff",
+  background: "linear-gradient(#000000d5,#00000089,#00000000)",
+})
+
+// Make a timeline to pin svg and change color of nav to black
+var tl = gsap.timeline({
+  scrollTrigger: {
+      trigger: "svg",
+      scroller: "#main",
+      // markers: true,
+      start: "top 0%",
+      end: "top -140%",
+      scrub: true,
+  }
+})
+
+tl.to("svg", {
+  scale: 1,
+  top: "5%",
+  fill: "#111",
+
 })
 
 
-t2.to("svg",{
-  scale:1,
-  top:"5%",
-  fill:"#fff",
+tl.to("#nav", {
+  color: "#111",
+  background: "linear-gradient(#ffffffeb,#ffffff6e,#ffffff00)",
 })
- t2.to("#nav",{
- color:"#fff",
+
+// Make a timeline again to change color of nav and svg to white
+
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+      trigger: "svg",
+      scroller: "#main",
+      // markers: "true",
+      start: "top -340%",
+      end: "top -340%",
+      scrub: true,
+  }
+})
+tl2.to("svg", {
+  scale: 1,
+  top: "5%",
+  fill: "#fff",
+
+})
+tl2.to("#nav", {
+  color: "#fff",
   background: "linear-gradient(#000000d5,#00000089,#00000000)",
 })
 
 
-
-gsap.from("#page2 h1",{
-  duration:0.5,
-  onStart: function() {
-    $('#page2 h1').textillate({ in: { effect: 'fadeInDown' } });
-  },
-})
 document.querySelector("#page4").addEventListener("mousemove", function (dets) {
   document.querySelector("#page4>img").style.left = (dets.x)+ "px"
   document.querySelector("#page4>img").style.top = (dets.y) + "px"
@@ -134,4 +212,105 @@ elem.forEach(function(e){
    e.addEventListener("mousemove",function(){
     document.querySelector("#page4>img").setAttribute("src",a)
    })
+})
+
+gsap.to("#page5", {
+  scrollTrigger: {
+      trigger: "#page5",
+      scroller: "#main",
+      start: "top -25%",
+      end: "top -100%",
+      scrub: true,
+      pin: true,
+      // markers: true
+  }
+})
+gsap.from("#div-2",{
+  y:500,
+  scrollTrigger:{
+      trigger:"#div-2",
+      scroller:"#main",
+      start:"top 80%",
+      end:"top 50%",
+      scrub:2,
+      markers:true
+  }
+})
+
+var scroll3 = document.querySelector("#scroll3")
+var page8 = document.querySelector("#page8")
+
+page8.addEventListener("mouseenter",function(){
+  scroll3.style.opacity = 1
+})
+page8.addEventListener("mouseleave",function(){
+  scroll3.style.opacity = 0
+})
+page8.addEventListener("mousemove",function(dets){
+  scroll3.style.left = `${dets.x+ 40}px`
+  scroll3.style.top = `${dets.y-35}px`
+})
+var btn = document.querySelector("#page9 #btn")
+var button1 = document.querySelector("#btn button")
+var white = document.querySelector(".white")
+btn.addEventListener("mouseenter",function(){
+  white.style.top = "0%"
+  button1.style.color = "#111"
+})
+btn.addEventListener("mouseleave",function(){
+  white.style.top = "100%"
+  button1.style.color = "#fff"
+})
+var btn1 = document.querySelector("#btn1")
+var button = document.querySelector("#btn1 button")
+var white1 = document.querySelector(".white1")
+btn1.addEventListener("mouseenter",function(){
+  white1.style.top = "0%"
+  button.style.color = "#111"
+})
+btn1.addEventListener("mouseleave",function(){
+  white1.style.top = "100%"
+  button.style.color = "#fff"
+})
+var button3 = document.querySelector(".nav-bar button")
+var white2 = document.querySelector(".white2")
+button3.addEventListener("mouseenter",function(){
+  white2.style.top = "0%"
+  button3.style.color = "#fff"
+})
+button3.addEventListener("mouseleave",function(){
+  white2.style.top = "100%"
+  button3.style.color = "#111"
+})
+
+var scroll4 = document.querySelector("#scroll4")
+var part1 = document.querySelector(".half-prt1")
+
+
+part1.addEventListener("mousemove",function(dets){
+
+  scroll4.style.opacity = `1`
+  scroll4.style.left = `${dets.x-70}px`
+  scroll4.style.top = `${dets.y-55}px`
+})
+part1.addEventListener("mouseleave",function(){
+  scroll4.style.opacity = `0`
+})
+var scroll5 = document.querySelector("#scroll5")
+var part2 = document.querySelector(".half-prt2")
+
+
+part2.addEventListener("mousemove",function(dets){
+  scroll5.style.left = `${dets.x}px`
+  scroll5.style.top = `${dets.y}px`
+})
+// part2.addEventListener("mouseleave",function(){
+//   scroll5.style.opacity = 0
+// })
+
+document.querySelector("#nav i").addEventListener("click",function(){
+  document.querySelector("#full-scr").style.top = "0vh"
+})
+document.querySelector("#full-scr i").addEventListener("click",function(){
+  document.querySelector("#full-scr").style.top = "-100vh"
 })
